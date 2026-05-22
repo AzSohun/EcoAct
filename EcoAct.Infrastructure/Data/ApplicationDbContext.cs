b6537c6858cb85e17a112ad8c4ba6fbcca21462c
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-22 22:38:26 +0600

    Fixed: The access modifier of the AppDbContext

--- EcoAct.Infrastructure/Data/ApplicationDbContext.cs
+++ EcoAct.Infrastructure/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace EcoAct.Infrastructure.Data
 {
-    internal class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {
---
+++ EcoAct.Infrastructure/Repositories/CampaignRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoAct.Infrastructure.Repositories
+{
+    internal class CampaignRepository
+    {
+    }
+}
