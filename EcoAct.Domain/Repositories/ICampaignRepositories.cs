1003cf3670d825da768ae9f4e81fb827d013e089
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-22 17:41:08 +0600

    Added: ICampaignRepository inteface

---
+++ EcoAct.Domain/Repositories/ICampaignRepositories.cs
@@ -0,0 +1,16 @@
+﻿using EcoAct.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoAct.Domain.Repositories
+{
+    public interface ICampaignRepositories
+    {
+        Task<IEnumerable<Campaign>> GetAllAsync();
+        Task<Campaign?> GetByIdAsync(Guid id);
+        Task CreateAsync(Campaign campaign);
+        Task UpdateAsync(Campaign campaign);
+        Task DeleteAsync(Guid id);
+    }
+}
