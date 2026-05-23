d77a466ad994864a174f5273af68702362e2d916
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-23 13:09:59 +0600

    Convert into monorepo

---
+++ backend/EcoAct.Domain/Repositories/ICampaignRepository.cs
@@ -0,0 +1,14 @@
+﻿using EcoAct.Domain.Entities;
+
+
+namespace EcoAct.Domain.Repositories
+{
+    public interface ICampaignRepository
+    {
+        Task<IEnumerable<Campaign>> GetAllAsync();
+        Task<Campaign?> GetByIdAsync(Guid id);
+        Task AddAsync(Campaign campaign);
+        Task UpdateAsync(Campaign campaign);
+        Task DeleteAsync(Guid id);
+    }
+}
